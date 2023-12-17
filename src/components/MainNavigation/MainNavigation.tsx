0c24cb35b2da7248c14bcb8cea7ec3eb0b5b7e0c
TymekGluch <78880779+TymekGluch@users.noreply.github.com>
2023-12-17 23:19:59 +0100

    refactor: change from scss to scss modules

--- src/components/MainNavigation/MainNavigation.tsx
+++ src/components/MainNavigation/MainNavigation.tsx
@@ -1,90 +1,93 @@
 import React from 'react';
-import './MainNavigation.scss';
+import styles from './MainNavigation.module.scss';
 import { useOutsideClick } from '../../hooks/useOutsideClick';
 import { listOfNavigationLink } from './MainNavigation.data';
+import classNames from 'classnames';
 
 const MainNavigation: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState<boolean>(false);
   const menuListRef = React.useRef<HTMLOListElement>(null);
 
-  const body = document.querySelector('body');
+  React.useEffect(() => {
+    if (isMenuOpen) {
+      document.body.classList.add('overflowHidden');
+    } else {
+      document.body.classList.remove('overflowHidden');
+    }
+  }, [isMenuOpen]);
 
   const handleClick = (): void => {
     setIsMenuOpen(true);
-
-    body?.classList.add('overflow-hidden');
   };
 
   const handleCloseButtonClick = (): void => {
     setIsMenuOpen(false);
-
-    body?.classList.remove('overflow-hidden');
   };
 
   const handleOutsideClick = (): void => {
-    body?.classList.remove('overflow-hidden');
-
     setIsMenuOpen(false);
   };
 
   useOutsideClick(handleOutsideClick, menuListRef);
 
+  console.log(styles, styles.mainNavigation);
+
   return (
-    <header className="main-navigation">
-      <a href="/" className="main-navigation__link">
+    <header className={styles.mainNavigation}>
+      <a href="/" className={styles.mainNavigation_link}>
         <img
-          className="main-navigation__logo"
+          className={styles.mainNavigation_logo}
           src="/src/assets/logo-bookmark.svg"
           alt="Bookmark - logo"
         />
       </a>
 
       <button
-        className={`main-navigation__toggle-menu${
-          isMenuOpen ? ' main-navigation__toggle-menu--turned-on' : ''
-        }`}
+        className={classNames(styles.mainNavigation_toggleMenu, {
+          [styles.maiNavigation_toggleMenu__turnedOn]: isMenuOpen,
+        })}
         onClick={handleClick}
       >
         <img src="/src/assets/icon-hamburger.svg" alt="open menu" />
       </button>
 
       <div
-        className={`main-navigation__menu${
-          isMenuOpen ? ' main-navigation__menu--open' : ''
-        } menu`}
+        className={classNames(styles.mainNavigation_menu, styles.menu, {
+          [styles.mainNavigation_menu__open]: isMenuOpen,
+        })}
       >
-        <header className="menu__header">
-          <a href="/" className="menu__link">
+        <header className={styles.menu_header}>
+          <a href="/" className={styles.menu_link}>
             <img
-              className="menu__logo"
+              className={styles.menu_logo}
               src="/src/assets/logo-bookmark-white.svg"
               alt="Bookmark - logo"
             />
           </a>
 
           <button
-            className="menu__close-button"
+            className={styles.menu_closeButton}
             onClick={handleCloseButtonClick}
           >
             <img src="/src/assets/icon-close.svg" alt="close menu" />
           </button>
         </header>
 
-        <ol ref={menuListRef} className="menu__list">
+        <ol ref={menuListRef} className={styles.menu_list}>
           {listOfNavigationLink.map(({ name, link }) => (
-            <li key={name} className="menu__list-item">
-              <a href={link} className="menu__item-link">
+            <li key={name} className={styles.menu_listItem}>
+              <a href={link} className={styles.menu_itemLink}>
                 {name}
               </a>
             </li>
           ))}
         </ol>
 
-        <button className="menu__login-button">Login</button>
+        <button className={styles.menu_loginButton}>Login</button>
 
-        <div className="menu__socials">
+        <div className={styles.menu_socials}>
           <a
-            className="menu__social-link"
+            className={styles.menu_socialLink}
             href=""
             aria-label="facebook"
             title="facebook"
@@ -95,7 +98,7 @@ const MainNavigation: React.FC = () => {
           </a>
 
           <a
-            className="menu__social-link"
+            className={styles.menu_socialLink}
             href="https://twitter.com/"
             aria-label="twitter"
             title="twitter"
